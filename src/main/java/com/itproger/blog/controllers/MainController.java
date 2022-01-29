91c75b04836bc39011ce73b9730bdcab25dd2868
Kizmari <kizmari@yandex.ru>
2022-01-29 12:20:36 +0300

    After Lesson 2 Gosha Dudar Java Spring Boot Lessons; После урока 2 Гоша Дударь Уроки Java Spring Boot;

--- src/main/java/com/itproger/blog/controllers/MainController.java
+++ src/main/java/com/itproger/blog/controllers/MainController.java
@@ -1,4 +1,26 @@
 package com.itproger.blog.controllers;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+
+@Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String home (Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+    @GetMapping("/about")
+    public String about (Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
 }
+
